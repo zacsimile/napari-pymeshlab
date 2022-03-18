666712ae4c157c10627fbb8c65f41f96f7f10e64
zacsimile <zach.marin@yale.edu>
2022-03-17 21:38:02 -0400

    Re #2

--- src/napari_pymeshlab/__init__.py
+++ src/napari_pymeshlab/__init__.py
@@ -2,7 +2,7 @@
 __version__ = "0.0.4"
 
 
-from ._reader import get_mesh_reader
+from ._reader import get_mesh_reader, mesh_reader
 from ._writer import write_single_surface  #, write_multiple 
 from ._sample_data import make_sphere, make_shell
 from ._widget import screened_poisson_reconstruction  # ExampleQWidget, example_magic_widget
