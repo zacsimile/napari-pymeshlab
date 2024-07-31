9e5f7606d8f81eae25ad8d8d51638a3ca9568969
zacsimile <zachcm@gmail.com>
2024-08-01 00:25:20 +0200

    Bump version

--- src/napari_pymeshlab/__init__.py
+++ src/napari_pymeshlab/__init__.py
@@ -1,13 +1,16 @@
+__version__ = "0.0.6"
 
-__version__ = "0.0.5"
 
-
-from ._reader import get_mesh_reader, mesh_reader
-from ._writer import write_single_surface  #, write_multiple 
-from ._sample_data import make_sphere, make_shell
-from ._widget import screened_poisson_reconstruction  # ExampleQWidget, example_magic_widget
-from ._widget import convex_hull, \
-                     laplacian_smooth, \
-                     taubin_smooth, \
-                     simplification_clustering_decimation, \
-                     colorize_curvature_apss
+from ._reader import get_mesh_reader, mesh_reader  # noqa
+from ._writer import write_single_surface  # noqa , write_multiple
+from ._sample_data import make_sphere, make_shell  # noqa
+from ._widget import (
+    screened_poisson_reconstruction,  # noqa
+)  # ExampleQWidget, example_magic_widget
+from ._widget import (
+    convex_hull,  # noqa
+    laplacian_smooth,  # noqa
+    taubin_smooth,  # noqa
+    simplification_clustering_decimation,  # noqa
+    colorize_curvature_apss,  # noqa
+)
